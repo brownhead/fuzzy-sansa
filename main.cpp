227ebf35465d97286fa463f6b13bff3dacd505d2
John Sullivan <j.sullivan@gmx.com>
2013-04-19 09:19:43 -0700

    Implemented print_map function.

--- main.cpp
+++ main.cpp
@@ -2,7 +2,28 @@
 
 using namespace std;
 
+void print_map(char map[80][80]) {
+	for (unsigned int i = 0; i < 80; ++i) {
+		for (unsigned int j = 0; j < 80; ++j) {
+			cout << map[i][j];
+		}
+		cout << endl;
+	}
+}
+
 int main()
 {
-	cout << "Hello World";
+	cout << "Welcome to fuzzy-sansa!";
+
+	char map[80][80];
+
+	for (unsigned int i = 0; i < 80; ++i) {
+		for (unsigned int j = 0; j < 80; ++j) {
+			map[i][j] = '.';
+		}
+	}
+
+	print_map(map);
+
+	return 0;
 }
