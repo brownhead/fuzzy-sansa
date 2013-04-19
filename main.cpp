3d131aecfbecebd6d3f27d6d9e81933f35b55a85
Alex Feinland <afeinlandlol@gmail.com>
2013-04-19 13:38:23 -0700

    Changed print_map function. Added structs.
    1) print_map now only displays viewing window around dude. Window size
    determined by view of type Size.
    
    2) struct Point created for holding position of the dude.

--- main.cpp
+++ main.cpp
@@ -2,18 +2,54 @@
 
 using namespace std;
 
-void print_map(char map[80][80]) {
-	for (unsigned int i = 0; i < 80; ++i) {
-		for (unsigned int j = 0; j < 80; ++j) {
-			cout << map[i][j];
-		}
-		cout << endl;
-	}
+struct Point{
+	int x;
+	int y;
+};
+
+struct Size{
+	int x;
+	int y;
+};
+
+
+void set_points(Point &dude, int x, int y){
+	dude.x = x;
+	dude.y = y;
 }
+
+void print_map(char map[80][80], Point dude_location, Size view) {
+
+	for (unsigned int i = dude_location.x - view.x/2; i <= dude_location.x + view.x/2; ++i) {
+		for (unsigned int j = dude_location.y - view.y/2; j <= dude_location.y + view.y/2; ++j) {
+			if(i == dude_location.x && j == dude_location.y){
+				cout  << '@' << " ";
+			}
+			else
+				cout << map[i][j] << " ";
+			}
+			cout << endl;
+		}
+	}
+
 
 int main()
 {
-	cout << "Welcome to fuzzy-sansa!";
+
+	Point dude_location;
+	int x = 40;
+	int y = 40;
+	set_points(dude_location, x, y);
+	cout << dude_location.x + 1 << " " << dude_location.y + 1 << endl;
+
+	Size view;
+	view.x = 6;
+	view.y = 6;
+
+
+
+
+	cout << "Welcome to fuzzy-sansa!\n";
 
 	char map[80][80];
 
@@ -23,7 +59,8 @@ int main()
 		}
 	}
 
-	print_map(map);
+	print_map(map, dude_location, view);
 
 	return 0;
 }
+
